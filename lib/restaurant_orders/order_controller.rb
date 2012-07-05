8656e1c8256bbf3fdf023c1098b9f17a061f482b
E. Sambo <news@emmanuelsambo.com>
2012-07-04 21:13:14 -0500

    Add spec #process sends message #get_combinations

--- lib/restaurant_orders/order_controller.rb
+++ lib/restaurant_orders/order_controller.rb
@@ -8,18 +8,32 @@ module RestaurantOrders
     end
 
     def call
-
+      process
     end
 
-    def parse
-      @input.each_line.map { |c|
-          c.chomp.split(',').tap { |e|
-            # e[0]  = e[0].downcase
-            e[-1] = e[-1].to_s[1..-1].to_f
-            # e[2]  = e[2].to_i
+      def process
+        parsed       = parse
+        target_price = parsed.shift.pop
+        dishes       = parsed
+        @menu        = new_menu(dishes)
+        @menu.get_combinations(target_price)
+      end
+
+        def new_menu(dishes)
+          menu_source.call(dishes)
+        end
+
+          def menu_source
+            @menu_source ||= Menu.public_method :new
+          end
+
+        def parse
+          @input.each_line.map { |c|
+            c.chomp.split(',').tap { |e|
+              e[-1] = e[-1].to_s[1..-1].to_f
+            }
           }
-        }
-    end
+        end
 
   end
 end
--- spec/restaurant_orders/order_controller_spec.rb
+++ spec/restaurant_orders/order_controller_spec.rb
@@ -12,9 +12,9 @@ module RestaurantOrders
     let(:input)      { double :Input }
     let(:output)     { double :Output }
     let(:controller) { OrderController.new(input, output) }
-    let(:menu)       { double :Menu }
+    let(:menu)       { double(:Menu).as_null_object }
     before :each do
-      controller.menu_source = ->{ menu }
+      controller.menu_source = ->(d){ menu }
     end
 
     describe '#parse' do
@@ -40,6 +40,23 @@ module RestaurantOrders
             ['mozzarella sticks', 4.20],
             ['sampler plate',     5.80]
           ]
+        end
+      end
+    end
+
+    describe '#process' do
+      context 'with parsed input' do
+        let(:target_price) { 4.90 }
+        before :each do
+          controller.stub(:parse) {[
+            [target_price],
+            ['mixed fruit',  2.15],
+            ['french fries', 2.75]
+          ]}
+        end
+        it 'sends message #get_combinations with target price' do
+          menu.should_receive(:get_combinations).with(target_price)
+          controller.process
         end
       end
     end
